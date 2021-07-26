aaa1838459de473c62c4a06d75eb1115da45b996
Hui Ren <huiren16@gmail.com>
2021-07-26 16:05:03 -0500

    Update apicalls.py

--- apicalls.py
+++ apicalls.py
@@ -1,20 +1,19 @@
+# import subprocess
 import requests
 
 #Specify a URL that resolves to your workspace
-URL = "http://127.0.0.1/"
-
-
+URL = "http://192.168.0.4:8000/"
 
 #Call each API endpoint and store the responses
-response1 = #put an API call here
-response2 = #put an API call here
-response3 = #put an API call here
-response4 = #put an API call here
+response1 = requests.get(URL).text
+response2 = requests.get(URL + 'score').text
+response3 = requests.get(URL + 'summary_stats').text
+response4 = requests.get(URL + 'timing').text
+json = {"lastmonth_activity": {"0": 234},"lastyear_activity": {"0": 3},"number_of_employees": {"0": 10}}
+response5 = requests.post(URL + 'predict', json = json).text
 
-#combine all API responses
-responses = #combine reponses here
+print(response1, response2, response3, response4, response5)
 
-#write the responses to your workspace
 
 
 
