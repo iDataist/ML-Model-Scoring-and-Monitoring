888080e7d2e5063ca4d7072e0aa1ed3a72f66d16
Hui Ren <huiren16@gmail.com>
2021-07-26 16:05:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,74 @@
-from flask import Flask, session, jsonify, request
+from flask import Flask, request, jsonify, render_template
+from flask.logging import create_logger
+import logging
 import pandas as pd
-import numpy as np
-import pickle
-import create_prediction_model
-import diagnosis 
-import predict_exited_from_saved_model
 import json
 import os
+import joblib
+import diagnostics
 
-
-
-######################Set up variables for use in our script
 app = Flask(__name__)
-app.secret_key = '1652d576-484a-49fd-913a-6879acfa6ba4'
+logging.basicConfig(
+    level=logging.INFO,
+    filename="app.log",
+    format="%(asctime)-15s %(message)s",
+)
+logger = logging.getLogger()
 
 with open('config.json','r') as f:
-    config = json.load(f) 
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'], config['ingested_data_file'])
+prediction_model = os.path.join(config['prod_deployment_path'], config['model'])
+test_records = os.path.join(config['prod_deployment_path'],
+                            config['test_records'])
 
-prediction_model = None
+@app.route("/")
+def home():
+    html = "<h3>Customer Churn Prediction Home</h3>"
+    return html.format(format)
 
+@app.route("/predict", methods=['POST'])
+def predict():
+    """Performs an sklearn prediction
+    input looks like:
+    {"lastmonth_activity": {
+            "0": 234},
+    "lastyear_activity": {
+            "0": 3},
+    "number_of_employees": {
+            "0": 10}}
+    result looks like:
+    { "prediction": [ 1 ] }
+    """
+    try:
+        clf = joblib.load(prediction_model)
+    except Exception as e:
+        logger.error(e)
+        return "Model not loaded"
+    json_payload = request.json
+    logger.info(f"JSON payload: {json_payload}")
+    inference_payload = pd.DataFrame(json_payload)
+    logger.info(f"inference payload DataFrame:{inference_payload}")
+    prediction = list(clf.predict(inference_payload))
+    prediction = [int(x) for x in prediction]
+    return jsonify({'prediction': prediction})
 
-#######################Prediction Endpoint
-@app.route("/prediction", methods=['POST','OPTIONS'])
-def predict():        
-    #call the prediction function you created in Step 3
-    return #add return value for prediction outputs
+@app.route("/score")
+def score():
+    with open(test_records, 'r') as file:
+        f1 = file.read().split(' ')[-2]
+        return jsonify({'f1_score': f1})
 
-#######################Scoring Endpoint
-@app.route("/scoring", methods=['GET','OPTIONS'])
-def stats():        
-    #check the score of the deployed model
-    return #add return value (a single F1 score number)
+@app.route("/summary_stats")
+def summary_stats():
+    df = pd.read_csv(dataset_csv_path).describe()
+    return render_template('simple.html', tables=[df.to_html()], titles=df.columns.values)
 
-#######################Summary Statistics Endpoint
-@app.route("/summarystats", methods=['GET','OPTIONS'])
-def stats():        
-    #check means, medians, and modes for each column
-    return #return a list of all calculated summary statistics
+@app.route("/timing")
+def timing():
+    times = diagnostics.execution_time()
+    return jsonify({'ingestion_time': times[0], 'training_time': times[1]})
 
-#######################Diagnostics Endpoint
-@app.route("/diagnostics", methods=['GET','OPTIONS'])
-def stats():        
-    #check timing and percent NA values
-    return #add return value for all diagnostics
-
-if __name__ == "__main__":    
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
