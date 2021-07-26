ccf8acb611b0533acd9e3757e2f6531ce64a12a8
Hui Ren <huiren16@gmail.com>
2021-07-26 16:05:36 -0500

    Update reporting.py

--- reporting.py
+++ reporting.py
@@ -1,32 +1,56 @@
-import pickle
-from sklearn.model_selection import train_test_split
 import pandas as pd
-import numpy as np
-from sklearn import metrics
+from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
-import seaborn as sns
 import json
 import os
+import joblib
 
-
-
-###############Load config.json and get path variables
 with open('config.json','r') as f:
-    config = json.load(f) 
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+deployed_model_path = os.path.join(config['prod_deployment_path'], config['model'])
+test_data_path = os.path.join(config['test_data_path'], config['test_data_file'])
+output_path = os.path.join(config['classification_report'])
 
-
-
-
-##############Function for reporting
 def score_model():
-    #calculate a confusion matrix using the test data and the deployed model
-    #write the confusion matrix to the workspace
+    model = joblib.load(deployed_model_path)
+    test_data = pd.read_csv(test_data_path)
+    X_test = test_data[['lastmonth_activity','lastyear_activity','number_of_employees']]
+    y_test = test_data['exited']
+    y_preds = model.predict(X_test)
+    return y_test, y_preds
 
 
+def classification_report_image(
+    y_test, y_preds, output_path
+):
 
+    """
+    produces classification report for testing results
+    input:
+            y_test:  test response values
+            y_test_preds: test predictions
+            output_path: path to store the figure
+    output:
+            None
+    """
+
+    plt.rc("figure", figsize=(7, 5))
+    plt.text(
+        0.01, 0.5, str("Test"), {"fontsize": 10}, fontproperties="monospace"
+    )
+    plt.text(
+        0.01,
+        0.1,
+        str(classification_report(y_test, y_preds)),
+        {"fontsize": 10},
+        fontproperties="monospace",
+    )
+    plt.axis("off")
+    plt.savefig(output_path)
+    plt.close()
 
 
 if __name__ == '__main__':
-    score_model()
+    y_test, y_preds = score_model()
+    classification_report_image(y_test, y_preds, output_path)
