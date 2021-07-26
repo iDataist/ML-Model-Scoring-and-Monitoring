a9e15cb2e22848a63bf37d6d7d8f15f3b26ea845
Hui Ren <huiren16@gmail.com>
2021-07-26 16:05:19 -0500

    Update fullprocess.py

--- fullprocess.py
+++ fullprocess.py
@@ -1,36 +1,54 @@
-
-
+import os
+import pandas as pd
+import json
+import ingestion
 import training
 import scoring
 import deployment
 import diagnostics
 import reporting
 
-##################Check and read new data
-#first, read ingestedfiles.txt
+with open('config.json','r') as f:
+    config = json.load(f)
+input_folder_path = config['input_folder_path']
+data_path = os.path.join(config['output_folder_path'], config['ingested_data_file'])
+model_path = os.path.join(config['output_model_path'], config['model'])
+deployed_model_path = os.path.join(config['prod_deployment_path'], config['model'])
+test_records = os.path.join(config['test_records'])
+deployed_test_records = os.path.join(config['prod_deployment_path'],
+                            config['test_records'])
+output_path = os.path.join(config['classification_report'])
 
-#second, determine whether the source data folder has files that aren't listed in ingestedfiles.txt
+ingested_files = pd.read_csv('ingested_files.txt', sep=' ', header=None)
+unique_ingested_files = set(ingested_files[1].unique())
+non_processed_files = set(os.listdir(input_folder_path)).difference(unique_ingested_files)
 
+if non_processed_files:
+    ingestion.merge_multiple_dataframe()
 
+    with open(deployed_test_records, 'r') as file:
+        f1 = file.read().split(' ')[-2]
 
-##################Deciding whether to proceed, part 1
-#if you found new data, you should proceed. otherwise, do end the process here
-
-
-##################Checking for model drift
-#check whether the score from the deployed model is different from the score from the model that uses the newest ingested data
-
-
-##################Deciding whether to proceed, part 2
-#if you found model drift, you should proceed. otherwise, do end the process here
-
-
-
-##################Re-deployment
-#if you found evidence for model drift, re-run the deployment.py script
-
-##################Diagnostics and reporting
-#run diagnostics.py and reporting.py for the re-deployed model
+    scoring.score_model(deployed_model_path, data_path, test_records)
+    with open(test_records, 'r') as file:
+        new_f1 = file.read().split(' ')[-2]
+    if new_f1 < f1:
+        print('new_f1 > f1')
+        training.train_model(data_path, model_path)
+        deployment.move_artifacts_to_production_deployment_dir()
+        y_test, y_preds = reporting.score_model()
+        reporting.classification_report_image(y_test, y_preds, output_path)
+        y_preds = diagnostics.model_predictions()
+        print('y_preds:')
+        print(y_preds)
+        summary =  diagnostics.dataframe_summary()
+        print('dataframe summary:')
+        print(summary)
+        info =  diagnostics.dataframe_info()
+        time =  diagnostics.execution_time()
+        print('execution time for ingestion and training:')
+        print(time)
+        diagnostics.outdated_packages_list()
 
 
 
