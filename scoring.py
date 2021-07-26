0fa2a3f2e827cf8139fcf0bb0744ea6bdd2dc20a
Hui Ren <huiren16@gmail.com>
2021-07-26 12:09:43 -0500

    Update scoring.py

--- scoring.py
+++ scoring.py
@@ -1,25 +1,33 @@
-from flask import Flask, session, jsonify, request
 import pandas as pd
-import numpy as np
-import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score
+import joblib
 import json
+from datetime import datetime
 
-
-
-#################Load config.json and get path variables
 with open('config.json','r') as f:
-    config = json.load(f) 
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'], config['ingested_data_file'])
+model_path = os.path.join(config['output_model_path'], config['model'])
+test_data_path = os.path.join(config['test_data_path'], config['test_data_file'])
+test_records = os.path.join(config['test_records'])
 
-
-#################Function for model scoring
 def score_model():
-    #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
-    #it should write the result to the latestscore.txt file
+    model = joblib.load(model_path)
+    test_data = pd.read_csv(test_data_path)
+    X_test = test_data[['lastmonth_activity','lastyear_activity','number_of_employees']]
+    y_test = test_data['exited']
+    y_preds = model.predict(X_test)
+    f1 = f1_score(y_test, y_preds)
 
+    dateTimeObj=datetime.now()
+    thetimenow=str(dateTimeObj.year)+ '/'+str(dateTimeObj.month)+ '/'+str(dateTimeObj.day)
+    allrecords=[model_path, test_data_path,len(test_data), thetimenow, f1]
+    with open(test_records,'a') as MyFile:
+        for element in allrecords:
+            MyFile.write(str(element) + " ")
+        MyFile.write('\n')
+
+if __name__ == '__main__':
+    score_model()
